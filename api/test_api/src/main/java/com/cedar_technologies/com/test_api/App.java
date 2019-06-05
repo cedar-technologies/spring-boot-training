0c0ecdde37d7cc3a0a1a35609d295cae04ce6435
alex <alexandre.jx@gmail.com>
2019-06-05 12:45:28 -0700

    setting up maven app

---
+++ api/test_api/src/main/java/com/cedar_technologies/com/test_api/App.java
@@ -0,0 +1,13 @@
+package com.cedar_technologies.com.test_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
